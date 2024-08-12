d52dd282a554c9ddc69bb1d55f311b7f0360439a
Bartek <bartlomiej.kacki@gmail.com>
2024-08-12 18:56:50 +0200

    Add project files.

---
+++ PowOfTwo/PowersOfTwo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowOfTwo
+{
+    internal class PowersOfTwo : IEnumerable<int>
+    {
+        public IEnumerator<int> GetEnumerator()
+        {
+            var maxPow = Math.Round(Math.Log(int.MaxValue, 2));
+            for(int i = 0; i<maxPow; i++)
+                yield return (int)Math.Pow(2, i);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public int this[int index]
+        {
+            get { return (int)Math.Pow(2, index); }
+        }
+    }
+}
---
+++ PowOfTwo/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace PowOfTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var powersOfTwo = new PowersOfTwo();
+            for(int i = 0; i<powersOfTwo.Count(); i++)
+                Console.WriteLine($"2^{i} = {powersOfTwo[i]}");
+        }
+    }
+}
